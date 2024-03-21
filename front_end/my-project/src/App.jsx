80eae9b8d53afe5be7ad11c976509162c6881ff1
andrewmarabante <andrewmarabante@gmail.com>
2024-03-20 18:20:21 -0700

    Created front end vite with tailwind

---
+++ front_end/my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-xl'>working</div>
+    </>
+  )
+}
+
+export default App
